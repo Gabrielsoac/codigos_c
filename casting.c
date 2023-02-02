494059231387c718aa76284e948f2615d3e591ae
Gabriel Soares <gabrielsoacc@gmail.com>
2023-02-02 08:35:37 -0300

    Aprendendo a fazer Casting em C

---
+++ casting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+        int x = 16;
+        int y = 3;
+        float resultado = (float) x / y;
+            
+             printf("%f", resultado);
+    
+    return 0;
+}
